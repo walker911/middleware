48b6a31bc927c3b8482b73c47b61fe29ec2e4f84
qinmu <qinmu911@163.com>
2020-07-17 17:52:40 +0800

    feat: add util

---
+++ server/src/main/java/com/debug/middleware/server/util/RedPacketUtil.java
@@ -0,0 +1,20 @@
+package com.debug.middleware.server.util;
+
+import java.util.List;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author mu qin
+ * @date 2020/7/17
+ */
+public class RedPacketUtil {
+
+    public static List<Integer> divideRedPacket(Integer totalAmount, Integer totalPeopleNum) {
+
+        return null;
+    }
+
+}
