6d7e4c4807a9c1702fec7aee68b27e228967849c
qinmu <qinmu911@163.com>
2020-07-17 16:22:48 +0800

    feat: update jackson config

--- server/src/main/java/com/debug/middleware/server/config/JacksonConfig.java
+++ server/src/main/java/com/debug/middleware/server/config/JacksonConfig.java
@@ -5,12 +5,19 @@ import com.debug.middleware.server.serialize.InstantSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.Formatter;
 
+import java.text.ParseException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 /**
  * <p>
@@ -30,8 +37,25 @@ public class JacksonConfig {
         // 使用 JSR310 提供的序列化/反序列化
         timeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
                 .addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        timeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        timeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern("HH:mm:ss")));
         objectMapper.registerModule(timeModule);
 
         return objectMapper;
+    }
+
+    @Bean
+    public Formatter<LocalDateTime> localDateTimeFormatter() {
+        return new Formatter<LocalDateTime>() {
+            @Override
+            public LocalDateTime parse(String text, Locale locale) throws ParseException {
+                return LocalDateTime.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            }
+
+            @Override
+            public String print(LocalDateTime object, Locale locale) {
+                return object.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            }
+        };
     }
 }
