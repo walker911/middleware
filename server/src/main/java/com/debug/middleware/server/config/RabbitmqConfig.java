09ba7159bbd1727eef341e60e9f6d1f8711073a0
qinmu <qinmu911@163.com>
2020-07-27 15:52:53 +0800

    feat: add rabbitmq

---
+++ server/src/main/java/com/debug/middleware/server/config/RabbitmqConfig.java
@@ -0,0 +1,15 @@
+package com.debug.middleware.server.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>
+ * RabbitMQ 配置
+ * </p>
+ *
+ * @author mu qin
+ * @date 2020/7/27
+ */
+@Configuration
+public class RabbitmqConfig {
+}
