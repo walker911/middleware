096542744791b70842435d4ec288c1664eed7f61
qinmu <qinmu911@163.com>
2020-08-21 18:09:57 +0800

    feat: add distribute lock

---
+++ server/src/main/java/com/debug/middleware/server/distribute/lock/LockOne.java
@@ -0,0 +1,15 @@
+package com.debug.middleware.server.distribute.lock;
+
+/**
+ * <p>
+ * 锁机制
+ * </p>
+ *
+ * @author mu qin
+ * @date 2020/8/21
+ */
+public class LockOne {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ server/src/main/java/com/debug/middleware/server/distribute/lock/SysConstant.java
@@ -0,0 +1,13 @@
+package com.debug.middleware.server.distribute.lock;
+
+/**
+ * <p>
+ * 系统常量类
+ * </p>
+ *
+ * @author mu qin
+ * @date 2020/8/21
+ */
+public class SysConstant {
+    public static Integer amount = 500;
+}
